1ef1d115e09b705a527354bb49bf68fa7c78eb37
lucasreis10 <lucaschristian10@gmail.com>
2022-05-02 10:36:33 -0300

    feat(usuario): adicionar nova regra de negocio em entidade

---
+++ src/main/kotlin/com/lucasreis10/arq/ref/domain/excepetion/DominioException.kt
@@ -0,0 +1,3 @@
+package com.lucasreis10.arq.ref.domain.excepetion
+
+class DominioException(exception: String): RuntimeException(exception)
--- src/main/kotlin/com/lucasreis10/arq/ref/domain/usuario/entity/Usuario.kt
+++ src/main/kotlin/com/lucasreis10/arq/ref/domain/usuario/entity/Usuario.kt
@@ -1,6 +1,7 @@
 package com.lucasreis10.arq.ref.domain.usuario.entity
 
 import com.lucasreis10.arq.ref.domain.emprestimo.entity.Emprestimo
+import com.lucasreis10.arq.ref.domain.excepetion.DominioException
 
 class Usuario(
     val matricula: String,
@@ -8,7 +9,13 @@ class Usuario(
     val email: String,
     val endereco: String,
     val telefone: String,
-    val emprestimos: List<Emprestimo>
+    val emprestimos: List<Emprestimo>,
 ) {
+
+    fun validar() {
+        if(this.matricula.length <= 3) {
+            DominioException("Matricula deve conter mais de 3 caracters")
+        }
+    }
 
 }
