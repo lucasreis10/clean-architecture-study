f61445511d88b8e1d43d7ab450c0bef8eaf45912
lucasreis10 <lucaschristian10@gmail.com>
2022-05-13 09:26:46 -0300

    config(sam.jvm.yaml): criar arquivo para quarkus'

--- src/main/kotlin/com/lucasreis10/arq/ref/infrastructure/config/usecase/UsesCaseConfig.kt
+++ src/main/kotlin/com/lucasreis10/arq/ref/infrastructure/config/usecase/UsesCaseConfig.kt
@@ -1,17 +1,30 @@
 package com.lucasreis10.arq.ref.infrastructure.config.usecase
 
+import com.lucasreis10.arq.ref.infrastructure.usuario.entrypoint.lambda.ObterUsuarioHandle
 import com.lucasreis10.arq.ref.infrastructure.usuario.repository.dynamodb.DynamoDbRepositoryUsuario
 import com.lucasreis10.arq.ref.usecase.usuario.obter.ObterUsuario
 import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.inject.Default
+import javax.inject.Inject
 import javax.ws.rs.Produces
 
 
 @ApplicationScoped
-class UsesCaseConfig(private val usuarioRepository: DynamoDbRepositoryUsuario) {
+class UsesCaseConfig() {
+
+    @Inject
+    @field: Default
+    private lateinit var usuarioRepository: DynamoDbRepositoryUsuario
 
     @Produces
+    @ApplicationScoped
     fun obterUsuario(): ObterUsuario {
         return ObterUsuario(usuarioRepository)
     }
 
+    @Produces
+    @ApplicationScoped
+    fun obterUsuarioLamda(): ObterUsuarioHandle {
+        return ObterUsuarioHandle()
+    }
 }
--- src/main/kotlin/com/lucasreis10/arq/ref/infrastructure/usuario/entrypoint/lambda/ObterUsuarioHandle.kt
+++ src/main/kotlin/com/lucasreis10/arq/ref/infrastructure/usuario/entrypoint/lambda/ObterUsuarioHandle.kt
@@ -4,20 +4,17 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse
+import com.lucasreis10.arq.ref.infrastructure.config.usecase.UsesCaseConfig
 import com.lucasreis10.arq.ref.usecase.usuario.obter.InputObterUsuarioDto
 import com.lucasreis10.arq.ref.usecase.usuario.obter.ObterUsuario
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import javax.enterprise.inject.Default
-import javax.inject.Inject
 
 class ObterUsuarioHandle: RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse>  {
 
-    @Inject
-    @field: Default
-    private lateinit var obterUsuario: ObterUsuario
-
     override fun handleRequest(event: APIGatewayV2HTTPEvent, context: Context): APIGatewayV2HTTPResponse {
+        val obterUsuario: ObterUsuario = UsesCaseConfig().obterUsuario()
+
         val logger = context.logger
 
         val command = try {
