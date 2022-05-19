29828193caaf7743e403a8446d0857cb31f2080e
lucasreis10 <lucaschristian10@gmail.com>
2022-05-19 00:08:05 -0300

    fix(usuario): ajustar import em UsuarioTestHelper

--- domain/src/test/kotlin/com/clean/architecture/domain/usuario/UsuarioTestHelper.kt
+++ domain/src/test/kotlin/com/clean/architecture/domain/usuario/UsuarioTestHelper.kt
@@ -1,7 +1,5 @@
 package com.clean.architecture.domain.usuario
 
-import com.clean.architecture.domain.domain.emprestimo.entity.Emprestimo
-
 
 object UsuarioTestHelper {
 
@@ -10,8 +8,8 @@ object UsuarioTestHelper {
                 email: String = "maria@email.com",
                 endereco: String = "Rua 1 lote 2 casa 3",
                 telefone: String = "99 3242-3242",
-    ): com.clean.architecture.domain.domain.usuario.entity.Usuario =
-        com.clean.architecture.domain.domain.usuario.entity.Usuario(
+    ): Usuario =
+        Usuario(
             matricula,
             nome,
             email,
