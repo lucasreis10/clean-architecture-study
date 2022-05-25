bf907270d74cc1dcb868f17f18c4bf32f0799350
lucasreis10 <lucaschristian10@gmail.com>
2022-05-25 12:10:52 -0300

    test: criar classe MockitoHelper para resolver problema de Kotlin com metodo Mockito.any()

---
+++ application/src/test/kotlin/MockitoHelper.kt
@@ -0,0 +1,19 @@
+import org.mockito.Mockito
+
+
+object MockitoHelper {
+    fun <T> anyObject(): T {
+        Mockito.any<T>()
+        return uninitialized()
+    }
+    @Suppress("UNCHECKED_CAST")
+    fun <T> uninitialized(): T =  null as T
+}
+
+
+
+
+
+
+
+
