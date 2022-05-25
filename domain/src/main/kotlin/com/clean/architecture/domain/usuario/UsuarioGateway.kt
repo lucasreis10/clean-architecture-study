ec8c10dee96c3c3b1e5ec3274b84fa50901b5826
lucasreis10 <lucaschristian10@gmail.com>
2022-05-24 23:25:10 -0300

    refactor(usuario-domain): renomear UsuarioRepository -> UsuarioGateway

--- domain/src/main/kotlin/com/clean/architecture/domain/usuario/UsuarioGateway.kt
+++ domain/src/main/kotlin/com/clean/architecture/domain/usuario/UsuarioGateway.kt
@@ -3,6 +3,8 @@ package com.clean.architecture.domain.usuario
 
 interface UsuarioRepository {
 
+    fun criarUsuario(usuario: Usuario): Usuario
+
     fun obterUsuario(matricula: String, nome: String): Usuario?
 
 }
